9e03aa24c32fd4be82fd5c4546f249244011d3d4
vanhung201 <vanhung.work@gmail.com>
2021-10-26 23:26:03 +0700

    HUNG UPDATE CREATE AND EDIT 26102021

--- QuanLyBDS/Areas/Admin/Controllers/PropertyAdminController.cs
+++ QuanLyBDS/Areas/Admin/Controllers/PropertyAdminController.cs
@@ -20,41 +20,34 @@ namespace QuanLyBDS.Areas.Admin.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            List<Property_Type> pt = model.Property_Type.ToList();
-            SelectList ptList = new SelectList(pt, "ID", "PROPERTY_TYPE_NAME");
-            ViewBag.PropertyTypeList = ptList;
-
-            List<District> district = model.Districts.ToList();
-            SelectList districtList = new SelectList(district, "ID", "DISTRICT_NAME");
-            ViewBag.DistrictList = districtList;
-
-            List<Property_Status> pst = model.Property_Status.ToList();
-            SelectList pstList = new SelectList(pst, "ID", "PROPERTY_STATUS_NAME");
-            ViewBag.PropertyStatusList = pstList;
+            ViewBag.PropertyTypeList = model.Property_Type.OrderByDescending(x => x.ID).ToList();
+            ViewBag.DistrictList = model.Districts.OrderByDescending(x => x.ID).ToList();
+            ViewBag.PropertyStatusList = model.Property_Status.OrderByDescending(x => x.ID).ToList();
+            ViewBag.InstallmentRate = "7,99";
 
             return View();
         }
         [HttpPost]
         public ActionResult Create(Property property)
         {
-            var pproperty = new Property();
+            var data = new Property();
 
-            pproperty.Property_Code = property.Property_Code;
-            pproperty.Property_Name = property.Property_Name;
-            pproperty.Description = property.Description;
-            pproperty.Address = property.Address;
-            pproperty.Area = property.Area;
-            pproperty.Bed_Room = property.Bed_Room;
-            pproperty.Bath_Room = property.Bath_Room;
-            pproperty.Price = property.Price;
-            pproperty.Installment_Rate = property.Installment_Rate;
-            pproperty.Avatar = property.Avatar;
-            pproperty.Album = property.Album;
-            pproperty.District_ID = property.District_ID;
-            pproperty.Property_Status_ID = property.Property_Status_ID;
-            pproperty.Property_Type_ID = property.Property_Type_ID;
+            data.Property_Code = property.Property_Code;
+            data.Property_Name = property.Property_Name;
+            data.Description = property.Description;
+            data.Address = property.Address;
+            data.Area = property.Area;
+            data.Bed_Room = property.Bed_Room;
+            data.Bath_Room = property.Bath_Room;
+            data.Price = property.Price;
+            data.Installment_Rate = property.Installment_Rate;
+            data.Avatar = property.Avatar;
+            data.Album = property.Album;
+            data.District_ID = property.District_ID;
+            data.Property_Status_ID = property.Property_Status_ID;
+            data.Property_Type_ID = property.Property_Type_ID;
 
-            model.Properties.Add(property);
+            model.Properties.Add(data);
             model.SaveChanges();
 
             return RedirectToAction("Index");
@@ -85,25 +78,21 @@ namespace QuanLyBDS.Areas.Admin.Controllers
         }
 
         [HttpGet]
-        public ActionResult Edit(int? id)
+        public ActionResult Edit(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
-            }
-            Property property = model.Properties.Find(id);
-            if (property == null)
-            {
-                return HttpNotFound();
-            }
+            var property = model.Properties.FirstOrDefault(x => x.ID == id);
+
+            ViewBag.PropertyTypeList = model.Property_Type.OrderByDescending(x => x.ID).ToList();
+            ViewBag.DistrictList = model.Districts.OrderByDescending(x => x.ID).ToList();
+            ViewBag.PropertyStatusList = model.Property_Status.OrderByDescending(x => x.ID).ToList();
+
             return View(property);
         }
 
-        [HttpPost, ActionName("Edit")]
-        [ValidateAntiForgeryToken]
-        public ActionResult EditConfirmed(int id, Property property)
+        [HttpPost]
+        public ActionResult Edit(int id, Property property)
         {
-            var data = model.Properties.Find(id);
+            var data = model.Properties.FirstOrDefault(x => x.ID == id);
 
             data.Property_Code = property.Property_Code;
             data.Property_Name = property.Property_Name;
